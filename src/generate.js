291fbc3d28c9a46645fec5429efd23245cd8fd1f
Mark Peace <mark.peace@neo4j.com>
2018-08-08 11:45:19 +0100

    Force required params to be passed

--- src/generate.js
+++ src/generate.js
@@ -1,5 +1,28 @@
-const x = () => {
-  return { ...{ x: "u" } };
-};
+import yargs from "yargs";
 
-console.log(x().x);
+const { repo, nextVersion, lastCommit, outputPrLinks = false } = yargs
+  .env("GITHUB_TOKEN")
+  .options({
+    repo: {
+      alias: "r",
+      describe: "Github repo to pull changes from",
+      demandOption: true
+    },
+    nextVersion: {
+      alias: "nv",
+      describe: "The next version of the software to be release",
+      demandOption: true
+    },
+    lastCommit: {
+      alias: "lc",
+      describe: "The last commit hash to be considered for the changelog",
+      demandOption: true
+    },
+    outputPrLinks: {
+      alias: "opl",
+      describe:
+        "Adds the corresponding Github link at the end of the change message"
+    }
+  })
+  .help()
+  .alias("h", "help").argv;
