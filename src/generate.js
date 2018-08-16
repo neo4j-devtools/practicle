8021987b07d2c1c3755778da39503fc496aa26b3
Mark Peace <mark.peace@neo4j.com>
2018-08-16 23:52:05 +0100

    List changelog prs in the major release series

--- src/generate.js
+++ src/generate.js
@@ -1,5 +1,6 @@
 import yargs from "yargs";
 import Octokit from "@octokit/rest";
+import semverSort from "semver-sort";
 
 /* functions */
 const extractChangeLog = obj => {
@@ -18,6 +19,14 @@ const extractFromGithubUrl = url => {
     repo,
     owner
   };
+};
+
+const getPrMessagesBetweenSha = (prs, from, to) => {
+  const indexFrom = prs.findIndex(_ => _.sha === from);
+  const indexTo = prs.findIndex(_ => _.sha === to);
+  const arr = prs
+    .splice(indexFrom <= 0 ? 0 : indexFrom, indexTo)
+    .filter(_ => _.pr);
 };
 
 const buildOutput = (logs, header, outputPrLinks) => {
@@ -65,6 +74,8 @@ const commandLineSetUp = () =>
     .alias("h", "help").argv;
 
 async function fetchAllReleases() {
+  const perPage = 30;
+
   async function getReleases(page) {
     const result = await octokit.repos.getTags({
       owner: repoInfo.owner,
@@ -86,15 +97,41 @@ async function fetchAllReleases() {
   return results.filter(_ => /^\d/.test(_.name));
 }
 
-async function getPullRequests() {
-  const result = await octokit.pullRequests.getAll({
+async function fetchCommitsBetween(from, to) {
+  const result = await octokit.repos.compareCommits({
     owner: repoInfo.owner,
     repo: repoInfo.repo,
-    state: "closed",
-    per_page: 100
+    base: from,
+    head: to
   });
 
-  const out = result.data
+  return result.data.commits;
+}
+
+async function getAllPullRequests() {
+  const perPage = 100;
+
+  async function getPullRequests(page) {
+    const result = await octokit.pullRequests.getAll({
+      owner: repoInfo.owner,
+      repo: repoInfo.repo,
+      state: "closed",
+      per_page: perPage,
+      page
+    });
+    return result.data;
+  }
+  let results = [];
+  let page = 1;
+  let stillGettingReleases = true;
+  while (stillGettingReleases) {
+    const data = await getPullRequests(page);
+    results = [...results, ...data];
+    stillGettingReleases = data.length === perPage;
+    page += 1;
+  }
+
+  return results
     .map(pr => {
       const changelogMessage = extractChangeLog(pr);
       const { url, merge_commit_sha, number } = pr;
@@ -108,18 +145,32 @@ async function getPullRequests() {
       }
     })
     .filter(_ => _);
-
-  return out.splice(0, out.indexOf(out.find(log => log.sha === lastCommit)));
-
-  // buildOutput(filteredLogs, nextVersion, outputPrLinks);
 }
 
-async function main() {
+async function main(lastCommit) {
+  const prs = await getAllPullRequests();
   const releases = await fetchAllReleases();
-  const prs = await getPullRequests();
-  releases.forEach(rel => {
-    console.log(`###${rel.name} (${rel.commit.sha})`);
-  });
+  const releaseTags = releases
+    .map(release => release.name)
+    .filter(name => name.startsWith(nextVersion.split(".")[0]));
+  releaseTags.push(nextVersion);
+  const sortedList = semverSort.desc(releaseTags);
+  for (const [index, value] of sortedList.entries()) {
+    const nextRelease = sortedList[index + 1];
+    const thisRelease = value === nextVersion ? lastCommit : value;
+    if (!value || !nextRelease) {
+      break;
+    }
+    const commits = await fetchCommitsBetween(nextRelease, thisRelease);
+    const commitsToPrs = commits
+      .map(commit => {
+        return prs.find(pr => pr.sha === commit.sha);
+      })
+      .filter(_ => _);
+    if (commitsToPrs.length !== 0) {
+      buildOutput(commitsToPrs, value, outputPrLinks);
+    }
+  }
 }
 
 /* main*/
@@ -134,11 +185,10 @@ const {
 
 const repoInfo = extractFromGithubUrl(repo);
 const octokit = new Octokit();
-const perPage = 30;
 
 octokit.authenticate({
   type: "token",
   token
 });
 
-main();
+main(lastCommit);
