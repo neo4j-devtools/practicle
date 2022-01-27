b6af7ac6c649ee849edd4fab5014e253d5222ddb
Jens Oknelid <jens.oknelid@gmail.com>
2022-01-27 21:56:01 +0100

    Lookup PR number in commit message (for cherry picks)

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -3,12 +3,14 @@ import semverCompare from "semver-compare";
 
 export const versionFilter = (name, prevVersion, nextVersion) => {
   const tokenizedNextVersion = nextVersion.split(".");
-  return prevVersion
+  const afterPrevVersion = prevVersion
     ? semverCompare(name, prevVersion) >= 0
     : name.startsWith(
         prevVersion ||
           [tokenizedNextVersion[0], tokenizedNextVersion[1]].join(".")
       );
+  const beforeNextVersion = semverCompare(name, nextVersion) <= 0;
+  return afterPrevVersion && beforeNextVersion;
 };
 
 export const isValidSemVer = str => semver.valid(str);
